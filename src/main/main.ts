5d3cae3a0cc2f3826b5ba0b122e6318df3e6d039
NjineS <stevenjine28@gmail.com>
2025-06-24 11:37:33 +0800

    Update

---
+++ src/main/main.ts
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from 'electron';
+import { join } from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      preload: join(__dirname, '../renderer/preload.js')
+    }
+  });
+
+  win.loadFile('public/index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
